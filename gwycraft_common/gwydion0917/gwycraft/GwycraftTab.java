6501bbfe11dddbf1dc4ba686980947c1b42b7f1c
gwydion0917 <keyespj@gmail.com>
2013-06-28 17:40:13 -0400

    Fixed Creative tab

--- gwycraft_common/gwydion0917/gwycraft/GwycraftTab.java
+++ gwycraft_common/gwydion0917/gwycraft/GwycraftTab.java
@@ -1,19 +1,24 @@
 package gwydion0917.gwycraft;
 
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.item.Item;
 
 public class GwycraftTab extends CreativeTabs
 {
-	public GwycraftTab(String label)
-	{
-		super(label);
-	}
-	
-	@Override
-	public Item getTabIconItem()
-	{
-		return Gwycraft.itemFlawedGems;
-	}	
+    GwycraftTab(String par2Str)
+    {
+        super(par2Str);
+    }
+
+    @SideOnly(Side.CLIENT)
+
+    /**
+     * the itemID for the item to be displayed on the tab
+     */
+    public int getTabIconItemIndex()
+    {
+        return ConfigGwycraft.blockGemCompressedID;
+    }
 }
