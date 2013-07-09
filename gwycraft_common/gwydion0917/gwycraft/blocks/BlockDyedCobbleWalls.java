87fed68f2728ab804787d2c2c2526378a7cc4395
Zepanv <zepanv@gmail.com>
2013-07-09 08:49:59 -0400

    Remove extra imports

--- gwycraft_common/gwydion0917/gwycraft/blocks/BlockDyedCobbleWalls.java
+++ gwycraft_common/gwydion0917/gwycraft/blocks/BlockDyedCobbleWalls.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockWall;
-import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.ItemStack;
