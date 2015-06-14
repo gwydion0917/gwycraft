801913514c567b0f87673ee478f0261cb1ab96e5
Glassmaker <JeanGlassmaker@mail.com>
2015-06-14 06:31:20 +0300

    Clean up

--- gwycraft_common/gwydion0917/gwycraft/Gwycraft.java
+++ gwycraft_common/gwydion0917/gwycraft/Gwycraft.java
@@ -9,21 +9,6 @@ import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.registry.GameRegistry;
-import gwydion0917.gwycraft.blocks.*;
-import gwydion0917.gwycraft.items.*;
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockBookshelf;
-import net.minecraft.block.material.Material;
-import net.minecraft.init.Blocks;
-import net.minecraft.init.Items;
-import net.minecraft.item.Item;
-import net.minecraft.item.Item.ToolMaterial;
-import net.minecraft.item.ItemStack;
-import net.minecraftforge.common.util.EnumHelper;
-import net.minecraftforge.oredict.OreDictionary;
-import net.minecraftforge.oredict.ShapedOreRecipe;
-import net.minecraftforge.oredict.ShapelessOreRecipe;
-
 
 @Mod(modid = "gwycraft", name = "GwyCraft", version = "0.1.9a", guiFactory = "gwydion0917.gwycraft.client.GwyCraftGuiFactory")
 public class Gwycraft {
@@ -53,14 +38,11 @@ public class Gwycraft {
         ConfigGwycraft.initConfig(event);
 
         GwycraftBlocks.init();
-        
         GwycraftItems.init();
 
         CommonProxy.registerRenderers();
 
         GwycraftRecipes.init();
-
-
        
         LogHelper.info("Pre-Init Complete");
     }
