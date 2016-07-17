8dc855092b993036767d75ba2abcf42d85aded85
JLBShecky <jlbshecky@gmail.com>
2016-07-17 15:06:15 -0400

    Updated Gem Ore Blocks
    And removed the associated unneeded itemBlock classes.

--- gwycraft_common/gwydion0917/gwycraft/blocks/BlockGemCompressed.java
+++ gwycraft_common/gwydion0917/gwycraft/blocks/BlockGemCompressed.java
@@ -2,30 +2,97 @@ package gwydion0917.gwycraft.blocks;
 
 import java.util.List;
 
+import javax.annotation.Nullable;
+
+import com.google.common.base.Function;
+
+import gwydion0917.gwycraft.Gwycraft;
+import gwydion0917.gwycraft.enums.EnumGemType;
 import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.properties.IProperty;
+import net.minecraft.block.properties.PropertyEnum;
+import net.minecraft.block.state.BlockStateContainer;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemMultiTexture;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+import gwydion0917.gwycraft.interfaces.MultiBlock;
 
-public class BlockGemCompressed extends Block {
-	/** The type of tree this log came from. */
-	public static final String[] gemBlockTextures = new String[] { "gwycraft:gemblock_quartz", "gwycraft:gemblock_citrine", "gwycraft:gemblock_tanzanite", "gwycraft:gemblock_sapphire", "gwycraft:gemblock_topaz", "gwycraft:gemblock_agate", "gwycraft:gemblock_garnet", "gwycraft:gemblock_moonstone", "gwycraft:gemblock_hematite", "gwycraft:gemblock_aquamarine",
-			"gwycraft:gemblock_amethyst", "gwycraft:gemblock_lapis_lazuli", "gwycraft:gemblock_tigerseye", "gwycraft:gemblock_emerald", "gwycraft:gemblock_ruby", "gwycraft:gemblock_onyx" };
+public class BlockGemCompressed extends Block implements MultiBlock {
+	public static PropertyEnum<EnumGemType> GEMTYPE = PropertyEnum.<EnumGemType> create("type", EnumGemType.class);
 
-	public BlockGemCompressed(Material mat) {
+	public BlockGemCompressed(Material mat, String name) {
 		super(mat);
 		this.setSoundType(SoundType.STONE);
+		this.setDefaultState(this.blockState.getBaseState().withProperty(GEMTYPE, EnumGemType.QUARTZ));
+		this.setLightLevel(1F);
+		this.setHardness(3.0F);
+		this.setResistance(5.0F);
+		this.setUnlocalizedName(name);
+		this.setRegistryName(name);
+		this.setCreativeTab(Gwycraft.tabs);
+
+		// Register the block and item
+		GameRegistry.register(this);
+
+		// GameRegistry.register(new ItemGemCompressed(this));
+		GameRegistry.register(new ItemMultiTexture(this, this, new Function<ItemStack, String>() {
+			@Nullable
+			public String apply(@Nullable ItemStack stack) {
+				return EnumGemType.byMetadata(stack.getItemDamage()).getUnlocalizedName();
+			}
+		}).setUnlocalizedName(name).setRegistryName(name));
 	}
 
 	@SideOnly(Side.CLIENT)
 	@Override
-	public void getSubBlocks(Item par1, CreativeTabs tab, List subItems) {
-		for (int i = 0; i < 16; i++) {
-			subItems.add(new ItemStack(this, 1, i));
+	public void getSubBlocks(Item itemIn, CreativeTabs tab, List subItems) {
+		for (EnumGemType enumGemType : EnumGemType.values()) {
+			subItems.add(new ItemStack(itemIn, 1, enumGemType.getMetadata()));
+		}
+	}
+
+	/**
+	 * Convert the given metadata into a BlockState for this Block
+	 */
+	@Override
+	public IBlockState getStateFromMeta(int meta) {
+		return this.getDefaultState().withProperty(GEMTYPE, EnumGemType.byMetadata(meta));
+	}
+
+	/**
+	 * Convert the BlockState into the correct metadata value
+	 */
+	@Override
+	public int getMetaFromState(IBlockState state) {
+		return ((EnumGemType) state.getValue(GEMTYPE)).getMetadata();
+	}
+
+	@Override
+	protected BlockStateContainer createBlockState() {
+		return new BlockStateContainer(this, new IProperty[] { GEMTYPE });
+	}
+
+	/**
+	 * Gets the metadata of the item this Block can drop. This method is called
+	 * when the block gets destroyed. It returns the metadata of the dropped
+	 * item based on the old metadata of the block.
+	 */
+	public int damageDropped(IBlockState state) {
+		return ((EnumGemType) state.getValue(GEMTYPE)).getMetadata();
+	}
+
+	public void registerRenders() {
+		for (EnumGemType enumGemType : EnumGemType.values()) {
+			ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(this), enumGemType.getMetadata(), new ModelResourceLocation(this.getRegistryName() + "_" + enumGemType.getName()));
 		}
 	}
 }
--- gwycraft_common/gwydion0917/gwycraft/blocks/BlockGemOre.java
+++ gwycraft_common/gwydion0917/gwycraft/blocks/BlockGemOre.java
@@ -3,37 +3,58 @@ package gwydion0917.gwycraft.blocks;
 import java.util.List;
 import java.util.Random;
 
+import javax.annotation.Nullable;
+
+import com.google.common.base.Function;
+
+import gwydion0917.gwycraft.Gwycraft;
 import gwydion0917.gwycraft.GwycraftItems;
+import gwydion0917.gwycraft.enums.EnumGemType;
+import gwydion0917.gwycraft.interfaces.MultiBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.properties.IProperty;
+import net.minecraft.block.properties.PropertyEnum;
+import net.minecraft.block.state.BlockStateContainer;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemMultiTexture;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
-public class BlockGemOre extends Block {
-	/** The type of tree this log came from. */
-	public static final String[] gemBlockTextures = new String[] { "gwycraft:ore_quartz", "gwycraft:ore_citrine", "gwycraft:ore_tanzanite", "gwycraft:ore_sapphire", "gwycraft:ore_topaz", "gwycraft:ore_agate", "gwycraft:ore_garnet", "gwycraft:ore_moonstone", "gwycraft:ore_hematite", "gwycraft:ore_aquamarine", "gwycraft:ore_amethyst",
-			"gwycraft:ore_lapis_lazuli", "gwycraft:ore_tigerseye", "gwycraft:ore_emerald", "gwycraft:ore_ruby", "gwycraft:ore_onyx" };
+public class BlockGemOre extends Block implements MultiBlock {
+	public static PropertyEnum<EnumGemType> GEMTYPE = PropertyEnum.<EnumGemType> create("type", EnumGemType.class);
 
-	public BlockGemOre(Material mat) {
+	public BlockGemOre(Material mat, String name) {
 		super(mat);
 		this.setSoundType(SoundType.STONE);
+		this.setHardness(3.0F);
+		this.setResistance(5.0F);
+		this.setUnlocalizedName(name);
+		this.setRegistryName(name);
+		this.setCreativeTab(Gwycraft.tabs);
+
+		// Register the block and item
+		GameRegistry.register(this);
+
+		// GameRegistry.register(new ItemGemCompressed(this));
+		GameRegistry.register(new ItemMultiTexture(this, this, new Function<ItemStack, String>() {
+			@Nullable
+			public String apply(@Nullable ItemStack stack) {
+				return EnumGemType.byMetadata(stack.getItemDamage()).getUnlocalizedName();
+			}
+		}).setUnlocalizedName(name).setRegistryName(name));
 	}
 
-	/*
 	@Override
-	public Item getItemDropped(int par1, Random par2Random, int par3) {
-		return GwycraftItems.itemEnchantedGems;
-		// ConfigGwycraft.itemEnchantedGemsID+256;
-	}
-	*/
-
-	@Override
-	public int quantityDroppedWithBonus(int par1, Random par2Random) {
-		return 1 + par2Random.nextInt(par1 + 1);
+	public int quantityDroppedWithBonus(int fortune, Random random) {
+		return 1 + random.nextInt(fortune + 1);
 	}
 
 	@Override
@@ -46,6 +67,33 @@ public class BlockGemOre extends Block {
 	public void getSubBlocks(Item par1, CreativeTabs tab, List subItems) {
 		for (int i = 0; i < 16; i++) {
 			subItems.add(new ItemStack(this, 1, i));
+		}
+	}
+
+	/**
+	 * Convert the given metadata into a BlockState for this Block
+	 */
+	@Override
+	public IBlockState getStateFromMeta(int meta) {
+		return this.getDefaultState().withProperty(GEMTYPE, EnumGemType.byMetadata(meta));
+	}
+
+	/**
+	 * Convert the BlockState into the correct metadata value
+	 */
+	@Override
+	public int getMetaFromState(IBlockState state) {
+		return ((EnumGemType) state.getValue(GEMTYPE)).getMetadata();
+	}
+
+	@Override
+	protected BlockStateContainer createBlockState() {
+		return new BlockStateContainer(this, new IProperty[] { GEMTYPE });
+	}
+
+	public void registerRenders() {
+		for (EnumGemType enumGemType : EnumGemType.values()) {
+			ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(this), enumGemType.getMetadata(), new ModelResourceLocation(this.getRegistryName() + "_" + enumGemType.getName()));
 		}
 	}
 }
