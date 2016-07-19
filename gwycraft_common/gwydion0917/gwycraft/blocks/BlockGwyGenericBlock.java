05809415d137e27be78efbac1af59fc7070b8297
JLBShecky <jlbshecky@gmail.com>
2016-07-18 23:56:34 -0400

    Generic Block Class that is used by a number most blocks.

--- gwycraft_common/gwydion0917/gwycraft/blocks/BlockGwyGenericBlock.java
+++ gwycraft_common/gwydion0917/gwycraft/blocks/BlockGwyGenericBlock.java
@@ -2,31 +2,115 @@ package gwydion0917.gwycraft.blocks;
 
 import java.util.List;
 
+import javax.annotation.Nullable;
+
+import com.google.common.base.Function;
+
+import gwydion0917.gwycraft.Gwycraft;
+import gwydion0917.gwycraft.interfaces.MultiItem;
 import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
+import net.minecraft.block.material.MapColor;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.properties.IProperty;
+import net.minecraft.block.properties.PropertyEnum;
+import net.minecraft.block.state.BlockStateContainer;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.EnumDyeColor;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemMultiTexture;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.RayTraceResult;
+import net.minecraft.world.World;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
-public class BlockGwyGenericBlock extends Block {
-	// textureName from constructor, ie: stonecobble
-	// private String tName;
+public class BlockGwyGenericBlock extends Block implements MultiItem {
+	public static final PropertyEnum<EnumDyeColor> COLOR = PropertyEnum.<EnumDyeColor>create("color", EnumDyeColor.class);
 
-	public BlockGwyGenericBlock(Material mat, SoundType sound) {
+	public BlockGwyGenericBlock(Material mat, SoundType sound, String name) {
 		super(mat);
 		this.setSoundType(sound);
+		this.setRegistryName(name);
+		this.setUnlocalizedName(name);
+		this.setCreativeTab(Gwycraft.tabs);
+		
+		// Register the block and item
+		GameRegistry.register(this);
 
+		// GameRegistry.register(new ItemGemCompressed(this));
+		GameRegistry.register(new ItemMultiTexture(this, this, new Function<ItemStack, String>() {
+			@Nullable
+			public String apply(@Nullable ItemStack stack) {
+				return EnumDyeColor.byMetadata(stack.getItemDamage()).getUnlocalizedName();
+			}
+		}).setUnlocalizedName(name).setRegistryName(name));
 	}
 
-	@SideOnly(Side.CLIENT)
+    /**
+     * Gets the metadata of the item this Block can drop. This method is called when the block gets destroyed. It
+     * returns the metadata of the dropped item based on the old metadata of the block.
+     */
+    public int damageDropped(IBlockState state) {
+        return ((EnumDyeColor)state.getValue(COLOR)).getMetadata();
+    }
+
+    /**
+     * returns a list of blocks with the same ID, but different meta (eg: wood returns 4 blocks)
+     */
+    @SideOnly(Side.CLIENT)
+    public void getSubBlocks(Item itemIn, CreativeTabs tab, List<ItemStack> list) {
+        for (EnumDyeColor enumdyecolor : EnumDyeColor.values()) {
+            list.add(new ItemStack(itemIn, 1, enumdyecolor.getMetadata()));
+        }
+    }
+
+    /**
+     * Get the MapColor for this Block and the given BlockState
+     */
+    public MapColor getMapColor(IBlockState state) {
+        return ((EnumDyeColor)state.getValue(COLOR)).getMapColor();
+    }
+
+    /**
+     * Convert the given metadata into a BlockState for this Block
+     */
+    public IBlockState getStateFromMeta(int meta) {
+        return this.getDefaultState().withProperty(COLOR, EnumDyeColor.byMetadata(meta));
+    }
+
+    /**
+     * Convert the BlockState into the correct metadata value
+     */
+    public int getMetaFromState(IBlockState state) {
+        return ((EnumDyeColor)state.getValue(COLOR)).getMetadata();
+    }
+
+    protected BlockStateContainer createBlockState() {
+        return new BlockStateContainer(this, new IProperty[] {COLOR});
+    }
+	
+	/**
+     * Called when a user uses the creative pick block button on this block
+     *
+     * @param target The full target the player is looking at
+     * @return A ItemStack to add to the player's inventory, Null if nothing should be added.
+     */
 	@Override
-	@SuppressWarnings({ "rawtypes", "unchecked" })
-	public void getSubBlocks(Item par1, CreativeTabs tab, List subItems) {
-		for (int i = 0; i < 16; i++) {
-			subItems.add(new ItemStack(this, 1, i));
+    public ItemStack getPickBlock(IBlockState state, RayTraceResult target, World world, BlockPos pos, EntityPlayer player) {
+		return new ItemStack(Item.getItemFromBlock(this), 1, state.getValue(COLOR).getMetadata());
+	}
+
+	@Override
+	public void registerRenders() {
+		for(EnumDyeColor color: EnumDyeColor.values()) {
+			ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(this), color.getMetadata(), new ModelResourceLocation(this.getRegistryName() + "_" + color.getName()));
 		}
 	}
 }
